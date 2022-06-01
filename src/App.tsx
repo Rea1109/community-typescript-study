9428efb57c2c5c28fa4c941dc161fa3bdb4376bf
rea <woohr1109@gmail.com>
2022-06-01 15:53:41 +0900

    [update] mok server by json-server lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
+    const handleData = () => {
+        fetch('http://localhost:3001/posts')
+            .then(res => res.json())
+            .then(data => console.log(data));
+    };
+
+    const createData = () => {
+        fetch('http://localhost:3001/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: 'Test',
+                content: 'I am testing!',
+                id: 41,
+            }),
+        }).then(res => console.log(res));
+    };
+
     return (
-        <BrowserRouter>
-            <div></div>
-        </BrowserRouter>
+        <div>
+            <button onClick={handleData}>데이터 가져오기</button>
+            <button onClick={createData}>데이터 등록해보기</button>
+        </div>
     );
 }
 
